dc04bbb1b5d0dbdcb81829ab3541bf6e72d962b5
robbbowes <up666724@myport.ac.uk>
2023-05-23 21:14:46 +0200

    Add project files.

---
+++ API/ApiEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace API;
+
+public static class ApiEndpoints
+{
+    private const string ApiBase = "api";
+
+    public static class Products
+    {
+        private const string Base = $"{ApiBase}/products";
+
+        public const string GetAll = Base;
+        public const string Get = $"{Base}/{{productId:int}}";
+    }
+
+}
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet(ApiEndpoints.Products.GetAll)]
+    public async Task<ActionResult<List<Product>>> GetAll()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpGet(ApiEndpoints.Products.Get)]
+    public async Task<ActionResult<Product>> Get(int productId)
+    {
+        return await _context.Products.FindAsync(productId);
+    }
+}
